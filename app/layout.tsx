5d2f538fcb7bb30b48940fc6fa539e2ad65dca4b
kylemacabasco <kylemacabasco@gmail.com>
2025-07-12 00:42:50 -0700

    styled the background

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Press_Start_2P } from "next/font/google";
 import "./globals.css";
 import { Web3Provider } from "./Web3Provider";
+import PixelBackground from "./components/PixelBackground";
+import Unicorns from "./components/Unicorns";
+import ParticleBackground from "./components/ParticleBackground";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +15,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const pressStart2P = Press_Start_2P({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-press-start-2p",
 });
 
 export const metadata: Metadata = {
@@ -27,8 +37,11 @@ export default function RootLayout({
     <html lang="en">
       <Web3Provider>
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} ${pressStart2P.variable} antialiased`}
         >
+          <ParticleBackground />
+          <PixelBackground />
+          <Unicorns />
           {children}
         </body>
       </Web3Provider>
